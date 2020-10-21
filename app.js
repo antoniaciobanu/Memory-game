3dc86eb295a95a2d0ba6ed58b5c8770e6bd89b4c
antoniaciobanu <ciobanuantonia18@yahoo.com>
2020-10-21 11:42:38 +0100

    Update

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
+class AudioController {
+    constructor() {
+        this.bgMusic = new Audio('audio/birds.mp3');
+        this.flipSound = new Audio('audio/flip.wav');
+        this.matchSound = new Audio('audio/match.wav');
+        this.victorySound = new Audio('audio/victory.wav');
+        this.gameOverSound = new Audio('audio/gameover.wav');
+        this.bgMusic.volume = 0.5;
+        this.bgMusic.loop = true;
+    }
+    startMusic() {
+        this.bgMusic.play();
+    }
+    stopMusic() {
+        this.bgMusic.pause();
+        this.bgMusic.currentTime = 0;
+    }
+    flip() {
+        this.flipSound.play();
+    }
+    match() {
+        this.matchSound.play();
+    }
+    victory() {
+        this.stopMusic();
+        this.victorySound.play();
+    }
+    gameOver() {
+        this.stopMusic();
+        this.gameOverSound.play();
+    }
+}
+
 class MixOrMatch {
     constructor(totalTime, cards) {
         this.cardsArray = cards;
@@ -5,7 +38,7 @@ class MixOrMatch {
         this.timeRemaining = totalTime;
         this.timer = document.getElementById('time-remaining');
         this.ticker = document.getElementById('flips');
-        
+        this.audioController = new AudioController();
     }
     startGame() {
         this.cardToCheck = null;
@@ -14,7 +47,7 @@ class MixOrMatch {
         this.matchedCards = [];
         this.busy = true;
         setTimeout(() => {
-            
+            this.audioController.startMusic();
             this.shuffleCards();
             this.countDown = this.startCountDown();
             this.busy = false;
@@ -31,7 +64,7 @@ class MixOrMatch {
     }
     flipCard(card) {
         if(this.canFlipCard(card)) {
-            
+            this.audioController.flip();
             this.totalClicks++;
             this.ticker.innerText = this.totalClicks;
             card.classList.add('visible');
@@ -55,7 +88,7 @@ class MixOrMatch {
         this.matchedCards.push(card2);
         card1.classList.add('matched');
         card2.classList.add('matched');
-        
+        this.audioController.match();
         if(this.matchedCards.length === this.cardsArray.length)
             this.victory(); 
     }
@@ -80,10 +113,12 @@ class MixOrMatch {
     }
     gameOver() {
         clearInterval(this.countDown);
+        this.audioController.gameOver();
         document.getElementById('game-over-text').classList.add('visible');
     }
     victory() {
         clearInterval(this.countDown);
+        this.audioController.victory();
         document.getElementById('victory-text').classList.add('visible');
     }
 
